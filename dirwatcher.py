37da7e208fac1971b68967f41c04e7cb710e9cff
dbmiddle <dbmiddle39@gmail.com>
2020-04-05 16:49:08 -0400

    Code refactoring and comment deletions

--- dirwatcher.py
+++ dirwatcher.py
@@ -26,9 +26,12 @@ current_dir = os.getcwd()
 
 def signal_handler(sig_num, frame):
     """
-    This is a handler for SIGTERM and SIGINT. Other signals can be mapped here as well (SIGHUP?)
-    Basically it just sets a global flag, and main() will exit it's loop if the signal is trapped.
-    :param sig_num: The integer signal number that was trapped from the OS.
+    This is a handler for SIGTERM and SIGINT. Other signals can be
+    mapped here as well (SIGHUP?)
+    Basically it just sets a global flag, and main()
+     will exit it's loop if the signal is trapped.
+    :param sig_num: The integer signal number that
+    was trapped from the OS.
     :param frame: Not used
     :return None
     """
@@ -38,21 +41,10 @@ def signal_handler(sig_num, frame):
 
 
 def watch_directory(args):
-    logger.info('Watching directory: {}, File Extension: {}, Polling Interval: {}, Magic Text: {}'
+    logger.info('''Watching directory: {}, File Extension: {},
+    Polling Interval: {}, Magic Text: {}'''
                 .format(args.path, args.ext, args.interval, args.magic
                         ))
-    # Keys are the actual filename and the values are where to begin searching
-    # Look at directory and get a list of files from it;
-    # Add those to dictionary if not already present and log it as a new file;
-    # Look through 'track_files' dict and compare that to a list of files
-    #   that is in the dictionary
-    # If file is not in your list anymore you have to log the
-    #   file and remove it from your dictionary
-    # Iterate through dictionary, open each file at the last line that you read
-    #   from
-    # Start reading from that point looking for any 'magic' text
-    # Update the last position that you read from in the dictionary
-
     while not exit_flag:
         try:
             # logger.info('Inside Watch Loop')
@@ -76,14 +68,15 @@ def find_magic(args):
     else:
         for target_file in full_file_path_list:
             if target_file not in file_logger:
-                logger.info('*** New file {} added to track_files dictionary ***'
+                logger.info('''*** New file {} added to
+                track_files dictionary ***'''
                             .format(target_file
                                     ))
                 file_logger.append(target_file)
             full_file_path = os.path.join(watched_dir_path, target_file)
             if search_file(args, full_file_path):
                 break
-        detect_removed_files(args)
+        detect_removed_files(args, full_file_path_list)
 
 
 def search_file(args, full_file_path):
@@ -94,7 +87,8 @@ def search_file(args, full_file_path):
             if args.magic in line:
                 if line_number >= track_files[full_file_path] and full_file_path in track_files.keys():
                     logger.info('*** Magic text {} found in {} on Line {} ***'
-                                .format(args.magic, full_file_path, line_number + 1
+                                .format(args.magic, full_file_path,
+                                        line_number + 1
                                         ))
                     track_files[full_file_path] += 1
 
@@ -109,11 +103,7 @@ def detect_added_files(args, line_number):
             track_files[target_file] = line_number
 
 
-def detect_removed_files(args):
-    watched_dir_path = os.path.join(current_dir, args.path)
-    file_list = os.listdir(watched_dir_path)
-    full_file_path_list = [os.path.join(current_dir, args.path, f)
-                           for f in file_list if f.endswith('.txt')]
+def detect_removed_files(args, full_file_path_list):
     for target_file in file_logger:
         if target_file not in full_file_path_list:
             del track_files[target_file]
@@ -135,11 +125,8 @@ def create_parser():
 
 
 def main():
-    # Hook these two signals from the OS ..
     signal.signal(signal.SIGINT, signal_handler)
     signal.signal(signal.SIGTERM, signal_handler)
-    # Now my signal_handler will get called if OS sends
-    # either of these to my process.
 
     app_start_time = dt.datetime.now()
 
@@ -156,9 +143,6 @@ def main():
     args = parser.parse_args()
 
     watch_directory(args)
-    # find_magic(args)
-    # detect_added_files(args)
-    # detect_removed_files(args)
 
     uptime = dt.datetime.now() - app_start_time
     logger.info(
@@ -171,9 +155,6 @@ def main():
 
     )
     logging.shutdown()
-
-    # EX. if ns.loglevel == info:
-    # logger.setLevel(logging.INFO)
 
 
 if __name__ == '__main__':
